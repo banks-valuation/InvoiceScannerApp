9ca0730efb9dbd75fffb64d7bd692f0427e5bd51
Banks Valuation <developer@banksvaluation.com>
2025-09-27 18:11:48 -0500

    Fix useState import in AuthCallback

--- src/components/AuthCallback.tsx
+++ src/components/AuthCallback.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MicrosoftService } from '../services/microsoftService';
 import { AlertModal } from './Modal';
